139e4f14e739066ba13efa6de4649718b05d4f16
ibamba <ikader737@gmail.com>
2020-09-09 18:32:12 +0200

    Interface of the functions

---
+++ attack.h
@@ -0,0 +1,152 @@
+#ifndef ATTACK_H_
+#define ATTACK_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <math.h>
+#include <stdint.h>
+#include <string.h>
+#include <time.h>
+
+//SIZE_TRACE is the total number of samples in each Trace
+#define SIZE_TRACE 6000
+
+//NB_TRACES is the total number of traces
+#define NB_TRACES 40
+
+// Use SUBINTERVAL1 and SUBINTERVAL2 to define a subinterval of samples to use the chosen attack (for example subinterval = [1000;3000] out of the 6000 samples)
+#define SUBINTERVAL1 0
+#define SUBINTERVAL2 (SIZE_TRACE - 1)
+
+//Program variables
+#define NB_SAMPLES (SUBINTERVAL2 - SUBINTERVAL1 + 1)
+#define NB_SUBKEYS 16
+#define POSSIBILITES 256
+
+static const int sbox[256] =
+	    {0x63, 0x7C, 0x77, 0x7B, 0xF2, 0x6B, 0x6F, 0xC5, 0x30, 0x01, 0x67, 0x2B, 0xFE, 0xD7, 0xAB, 0x76,
+            0xCA, 0x82, 0xC9, 0x7D, 0xFA, 0x59, 0x47, 0xF0, 0xAD, 0xD4, 0xA2, 0xAF, 0x9C, 0xA4, 0x72, 0xC0,
+            0xB7, 0xFD, 0x93, 0x26, 0x36, 0x3F, 0xF7, 0xCC, 0x34, 0xA5, 0xE5, 0xF1, 0x71, 0xD8, 0x31, 0x15,
+            0x04, 0xC7, 0x23, 0xC3, 0x18, 0x96, 0x05, 0x9A, 0x07, 0x12, 0x80, 0xE2, 0xEB, 0x27, 0xB2, 0x75,
+            0x09, 0x83, 0x2C, 0x1A, 0x1B, 0x6E, 0x5A, 0xA0, 0x52, 0x3B, 0xD6, 0xB3, 0x29, 0xE3, 0x2F, 0x84,
+            0x53, 0xD1, 0x00, 0xED, 0x20, 0xFC, 0xB1, 0x5B, 0x6A, 0xCB, 0xBE, 0x39, 0x4A, 0x4C, 0x58, 0xCF,
+            0xD0, 0xEF, 0xAA, 0xFB, 0x43, 0x4D, 0x33, 0x85, 0x45, 0xF9, 0x02, 0x7F, 0x50, 0x3C, 0x9F, 0xA8,
+            0x51, 0xA3, 0x40, 0x8F, 0x92, 0x9D, 0x38, 0xF5, 0xBC, 0xB6, 0xDA, 0x21, 0x10, 0xFF, 0xF3, 0xD2,
+            0xCD, 0x0C, 0x13, 0xEC, 0x5F, 0x97, 0x44, 0x17, 0xC4, 0xA7, 0x7E, 0x3D, 0x64, 0x5D, 0x19, 0x73,
+            0x60, 0x81, 0x4F, 0xDC, 0x22, 0x2A, 0x90, 0x88, 0x46, 0xEE, 0xB8, 0x14, 0xDE, 0x5E, 0x0B, 0xDB,
+            0xE0, 0x32, 0x3A, 0x0A, 0x49, 0x06, 0x24, 0x5C, 0xC2, 0xD3, 0xAC, 0x62, 0x91, 0x95, 0xE4, 0x79,
+            0xE7, 0xC8, 0x37, 0x6D, 0x8D, 0xD5, 0x4E, 0xA9, 0x6C, 0x56, 0xF4, 0xEA, 0x65, 0x7A, 0xAE, 0x08,
+            0xBA, 0x78, 0x25, 0x2E, 0x1C, 0xA6, 0xB4, 0xC6, 0xE8, 0xDD, 0x74, 0x1F, 0x4B, 0xBD, 0x8B, 0x8A,
+            0x70, 0x3E, 0xB5, 0x66, 0x48, 0x03, 0xF6, 0x0E, 0x61, 0x35, 0x57, 0xB9, 0x86, 0xC1, 0x1D, 0x9E,
+            0xE1, 0xF8, 0x98, 0x11, 0x69, 0xD9, 0x8E, 0x94, 0x9B, 0x1E, 0x87, 0xE9, 0xCE, 0x55, 0x28, 0xDF,
+            0x8C, 0xA1, 0x89, 0x0D, 0xBF, 0xE6, 0x42, 0x68, 0x41, 0x99, 0x2D, 0x0F, 0xB0, 0x54, 0xBB, 0x16};
+            
+            
+            
+static const int possible_keys[256] = 
+		{0 , 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10 , 11 , 12 , 13 , 14 , 15 , 16 , 17 , 18 , 19 , 20 , 
+			21 , 22 , 23 , 24 , 25 , 26 , 27 , 28 , 29 , 30 , 31 , 32 , 33 , 34 , 35 , 36 , 37 , 38 , 
+			39 , 40 , 41 , 42 , 43 , 44 , 45 , 46 , 47 , 48 , 49 , 50 , 51 , 52 , 53 , 54 , 55 , 56 , 
+			57 , 58 , 59 , 60 , 61 , 62 , 63 , 64 , 65 , 66 , 67 , 68 , 69 , 70 , 71 , 72 , 73 , 74 , 75 , 
+			76 , 77 , 78 , 79 , 80 , 81 , 82 , 83 , 84 , 85 , 86 , 87 , 88 , 89 , 90 , 91 , 92 , 93 , 94 , 95 , 
+			96 , 97 , 98 , 99 , 100 , 101 , 102 , 103 , 104 , 105 , 106 , 107 , 108 , 109 , 110 , 111 , 112 , 113 , 
+			114 , 115 , 116 , 117 , 118 , 119 , 120 , 121 , 122 , 123 , 124 , 125 , 126 , 127 , 128 , 129 , 130 , 131 , 
+			132 , 133 , 134 , 135 , 136 , 137 , 138 , 139 , 140 , 141 , 142 , 143 , 144 , 145 , 146 , 147 , 148 , 149 , 150 ,
+			151 , 152 , 153 , 154 , 155 , 156 , 157 , 158 , 159 , 160 , 161 , 162 , 163 , 164 , 165 , 166 , 167 , 168 , 169 , 
+			170 , 171 , 172 , 173 , 174 , 175 , 176 , 177 , 178 , 179 , 180 , 181 , 182 , 183 , 184 , 185 , 186 , 187 , 188 ,
+			189 , 190 , 191 , 192 , 193 , 194 , 195 , 196 , 197 , 198 , 199 , 200 , 201 , 202 , 203 , 204 , 205 , 206 , 207 ,
+			208 , 209 , 210 , 211 , 212 , 213 , 214 , 215 , 216 , 217 , 218 , 219 , 220 , 221 , 222 , 223 , 224 , 225 , 226 , 
+			227 , 228 , 229 , 230 , 231 , 232 , 233 , 234 , 235 , 236 , 237 , 238 , 239 , 240 , 241 , 242 , 243 , 244 , 245 , 
+			246 , 247 , 248 , 249 , 250 , 251 , 252 , 253 , 254 , 255 };	
+			
+/**
+ * Find the max value of the set
+ * @param myArray : array to find max value
+ * @param size : the size of the array
+ * 
+*/
+inline double maxValue( double myArray [], int size)
+{
+    int i;
+    double maxValue;
+    maxValue=myArray[0];
+
+    //find the largest no
+    for (i=0;i<size;i++)
+    {
+    	if(myArray[i]>maxValue)
+    	    maxValue=myArray[i];
+    }
+    return maxValue;
+}
+
+/**
+ *Find the index of the max value of the set 
+ * @param myArray : array to find max value
+ * @param size : the size of the array
+ * 
+*/
+inline int ArgMax(double myArray [], int size){
+	int argmax = 0;
+	double maxValue = myArray[0];
+
+	for (int i=0;i<size;i++)
+    {
+    	if(myArray[i]>maxValue){
+    	    maxValue=myArray[i];
+    	    argmax = i;
+		}
+    }
+	return argmax;
+}
+
+/**
+ * Hamming distance between integers a and b
+ * @param a
+ * @param b
+ * 
+*/
+inline int HammingDistance(int a,int b){
+	int x = a ^ b;
+	int setBits = 0;
+	while (x > 0) {
+		setBits += x & 1;
+		x >>= 1;
+	}
+  	return setBits;
+}
+
+/**
+ * Hamming weight of an integer x
+ * @param x
+ * 
+*/
+inline int hamming_weight(int x){
+	int HammingWeight = 0 ;
+	while (x > 0)
+	{
+		HammingWeight += x & 1 ;
+		x = x >> 1 ;
+	}
+	return HammingWeight ;
+}
+
+
+//CPA Functions
+double correlationPearson(double tableauX[],double ** traces, int instant, int tailleTableau);
+double correlation_max_key_byte(int key_byte,uint8_t** plaintexts, int ind_byte,double** traces);
+void cpa_attack(uint8_t ** plain_texts, double ** traces, int * key_estimation);
+
+
+//DPA Functions
+int get_bit(uint8_t p, int ind_bit);
+void add(double * g, double * t);
+void average(double * g, int nb);
+void diff(double * g1, double * g2, double * res);
+void dpa_attack(uint8_t ** plain_texts, double ** traces, int * key_estimation, int ind_bit);
+
+
+#endif
